cf44f9335e40b4f68a7b2ad4d09fa7211b5a2ce5
ashhanai <ashhab.naim@gmail.com>
2023-03-31 16:14:38 +0200

    Implement ATREnabled1155

---
+++ src/ATREnabled1155.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import { ERC1155 } from "@openzeppelin/token/ERC1155/ERC1155.sol";
+import { ATRToken } from "./ATRToken.sol";
+
+
+/**
+ * - Owner or approved address can transfer only untokenized amount
+ * - Tokenized amount can be transferred only by ATR token holder via `atrTransferFrom` function
+ */
+contract ATREnabled1155 is ERC1155 {
+
+    // # Invariants
+    // - address balance is >= total supply of user atr id (atr id = hash(user address, token id) )
+
+    ATRToken public immutable atr;
+
+
+    constructor() ERC1155("metadata:uri") {
+        atr = new ATRToken("metadata:uri", address(this));
+    }
+
+
+    // # mint / burn ATR token
+
+    function mintTransferRights(uint256 tokenId, uint256 amount) external {
+        // For ERC1155, ATR id is owner address hashed with token id
+        // It enables fungibility of tokens locked in one address
+        uint256 atrId = _atrId(msg.sender, tokenId);
+
+        uint256 balance = balanceOf(msg.sender, tokenId);
+        uint256 atrBalance = atr.balanceOf(msg.sender, atrId);
+        require(balance - atrBalance >= amount, "Insufficient untokenized balance");
+
+        atr.mint(msg.sender, atrId, amount);
+    }
+
+    function burnTransferRights(uint256 tokenId, uint256 amount) external {
+        uint256 atrId = _atrId(msg.sender, tokenId);
+
+        uint256 atrBalance = atr.balanceOf(msg.sender, atrId);
+        require(atrBalance >= amount, "Insufficient tokenized balance");
+
+        atr.burn(msg.sender, atrId, amount);
+    }
+
+
+    // # use transfer rights
+
+    function atrTransferFrom(address from, address to, uint256 tokenId, uint256 amount, bool burnAtr) external {
+        uint256 atrId = _atrId(from, tokenId);
+
+        uint256 atrBalance = atr.balanceOf(msg.sender, atrId);
+        require(atrBalance >= amount, "Insufficient atr balance");
+
+        atr.burn(msg.sender, atrId, amount);
+        if (!burnAtr)
+            // need to mint a new atr tokens when owner changes
+            atr.mint(msg.sender, _atrId(to, tokenId), amount);
+
+        _safeTransferFrom(from, to, tokenId, amount, "");
+    }
+
+
+    // # transfer constraints
+
+    function _beforeTokenTransfer(
+        address /*operator*/,
+        address from,
+        address /*to*/,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory /*data*/
+    ) override internal view {
+        if (from == address(0))
+            return; // mint ATREnabled1155 tokens
+
+        for (uint256 i = 0; i < ids.length; ++i) {
+            uint256 id = ids[i];
+            uint256 amount = amounts[i];
+
+            uint256 atrId = _atrId(from, id);
+            // Number of all minted atr tokens with this id is equal to the number of tokenized assets in the address
+            uint256 atrTotalSupply = atr.totalSupply(atrId);
+            uint256 balance = balanceOf(from, id);
+            require(balance - atrTotalSupply >= amount, "Insufficient untokenized balance");
+        }
+    }
+
+
+    // # helpers
+
+    function mint(address account, uint256 tokenId, uint256 amount) external {
+        _mint(account, tokenId, amount, "");
+    }
+
+    function burn(address account, uint256 tokenId, uint256 amount) external {
+        _burn(account, tokenId, amount);
+    }
+
+
+    function _atrId(address owner, uint256 tokenId) private pure returns (uint256) {
+        return uint256(keccak256(abi.encode(owner, tokenId)));
+    }
+
+}
---
+++ test/ATREnabled1155.t.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+
+import { ATRToken } from "../src/ATRToken.sol";
+import { ATREnabled1155 } from "../src/ATREnabled1155.sol";
+
+
+contract ATREnabled1155Test is Test {
+
+    ATRToken atr;
+    ATREnabled1155 asset;
+
+    address joey = makeAddr("joey");
+    address chandler = makeAddr("chandler");
+    address ross = makeAddr("ross");
+    uint256 tokenId = 42;
+
+    function setUp() external {
+        asset = new ATREnabled1155();
+        atr = asset.atr();
+
+        asset.mint(joey, tokenId, 100);
+    }
+
+    function _atrId(address owner, uint256 _tokenId) private pure returns (uint256) {
+        return uint256(keccak256(abi.encode(owner, _tokenId)));
+    }
+
+
+    // mint
+
+    function test_shouldMintATRToken() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        assertEq(atr.balanceOf(joey, _atrId(joey, tokenId)), 100);
+    }
+
+    function test_shouldFailToMintATRToken_whenInsufficientBalance() external {
+        vm.expectRevert("Insufficient untokenized balance");
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 110);
+    }
+
+    function test_shouldFailToMintATRToken_whenInsufficientUntokenizedBalance() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 60);
+
+        vm.expectRevert("Insufficient untokenized balance");
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 60);
+    }
+
+
+    // burn
+
+    function test_shouldBurnATRToken() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        vm.prank(joey);
+        asset.burnTransferRights(tokenId, 100);
+
+        assertEq(atr.balanceOf(joey, _atrId(joey, tokenId)), 0);
+    }
+
+    function test_shouldFailToBurn_whenInsufficientTokenizedBalance_1() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        vm.prank(joey);
+        atr.safeTransferFrom(joey, chandler, _atrId(joey, tokenId), 50, "");
+
+        assertEq(atr.balanceOf(joey, _atrId(joey, tokenId)), 50);
+        assertEq(atr.balanceOf(chandler, _atrId(joey, tokenId)), 50);
+
+        vm.expectRevert("Insufficient tokenized balance");
+        vm.prank(joey);
+        asset.burnTransferRights(tokenId, 60);
+
+        vm.expectRevert("Insufficient tokenized balance");
+        vm.prank(joey);
+        asset.burnTransferRights(tokenId, 90);
+    }
+
+    function test_shouldFailToBurn_whenInsufficientTokenizedBalance_2() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 80);
+
+        vm.expectRevert("Insufficient tokenized balance");
+        vm.prank(joey);
+        asset.burnTransferRights(tokenId, 90);
+    }
+
+
+    // transfer
+
+    function test_shouldTransfer_whenSufficientUntokenizedBalance() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 80);
+
+        vm.prank(joey);
+        asset.safeTransferFrom(joey, chandler, tokenId, 20, "");
+
+        assertEq(asset.balanceOf(joey, tokenId), 80);
+        assertEq(asset.balanceOf(chandler, tokenId), 20);
+    }
+
+    function test_shouldFailToTransfer_whenInsufficientUntokenizedBalance() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 80);
+
+        vm.expectRevert("Insufficient untokenized balance");
+        vm.prank(joey);
+        asset.safeTransferFrom(joey, chandler, tokenId, 30, "");
+    }
+
+
+    // transfer via atr
+
+    function test_shouldTransfer_whenSufficientTokenizedBalance() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        vm.prank(joey);
+        atr.safeTransferFrom(joey, chandler, _atrId(joey, tokenId), 50, "");
+
+        assertEq(atr.balanceOf(chandler, _atrId(joey, tokenId)), 50);
+
+        vm.prank(chandler);
+        asset.atrTransferFrom(joey, ross, tokenId, 50, true);
+
+        assertEq(asset.balanceOf(joey, tokenId), 50);
+        assertEq(asset.balanceOf(ross, tokenId), 50);
+    }
+
+    function test_shouldFail_whenInsufficientATRBalance() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        vm.prank(joey);
+        atr.safeTransferFrom(joey, chandler, _atrId(joey, tokenId), 50, "");
+
+        vm.expectRevert("Insufficient atr balance");
+        vm.prank(chandler);
+        asset.atrTransferFrom(joey, ross, tokenId, 60, true);
+    }
+
+    function test_shouldBurnFromATRToken() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        vm.prank(joey);
+        atr.safeTransferFrom(joey, chandler, _atrId(joey, tokenId), 50, "");
+
+        assertEq(atr.balanceOf(chandler, _atrId(joey, tokenId)), 50);
+
+        vm.prank(chandler);
+        asset.atrTransferFrom(joey, ross, tokenId, 50, true);
+
+        assertEq(atr.balanceOf(joey, _atrId(joey, tokenId)), 50);
+        assertEq(atr.balanceOf(chandler, _atrId(joey, tokenId)), 0);
+    }
+
+    function test_shouldMintToATRToken_whenNotBurnFlag() external {
+        vm.prank(joey);
+        asset.mintTransferRights(tokenId, 100);
+
+        vm.prank(joey);
+        atr.safeTransferFrom(joey, chandler, _atrId(joey, tokenId), 50, "");
+
+        assertEq(atr.balanceOf(chandler, _atrId(joey, tokenId)), 50);
+
+        vm.prank(chandler);
+        asset.atrTransferFrom(joey, ross, tokenId, 50, false);
+
+        assertEq(atr.balanceOf(joey, _atrId(joey, tokenId)), 50);
+        assertEq(atr.balanceOf(chandler, _atrId(joey, tokenId)), 0);
+        assertEq(atr.balanceOf(chandler, _atrId(ross, tokenId)), 50);
+    }
+
+}
